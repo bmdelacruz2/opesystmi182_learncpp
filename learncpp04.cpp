5128ba3c090bc86c64508678d79c7dc2c0b26da5
âBrian Dela Cruz <bmdelacruz2@student.apc.edu.ph>
2019-11-28 02:28:30 +0000

    learncpp04 final commit

--- learncpp04.cpp
+++ learncpp04.cpp
@@ -4,22 +4,23 @@
    #include<iomanip>
    using namespace std;
    int main()
-   { 
-   int i,j;    
-   for(i = 1;i <= 5;++i)
+   {
+   int i,j;
+   for(i = 1;i <= 10;++i)
    {
 
 
-       for(j = 1;j <= 10;++j)
+       for(j = 1;j <= 5;++j)
           cout<<setw(4)<<j*i;
 
        cout<<endl;
-       
 
-       for(j = 2;j <= 10;++j)
+
+       for(j = 2;j <= 5;++j)
 
        cout<<endl;
 
    }
    return 0;
    }
+
